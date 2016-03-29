a09b613f9b21dbc262c6bf15c417b6dcca8e7bf5
pupi1985 <pupi1985@bitbucket.org>
2016-03-29 00:12:47 -0300

    Add dictionaries table migration

---
+++ database/migrations/2016_03_29_030927_create_dictionaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDictionariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dictionaries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('parts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dictionaries');
+    }
+}
